344294218424965d23618773e72dcfb2344bdab5
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-29 15:07:05 +0530

    React-js

---
+++ app/src/Component/ClassOneComp.jsx
@@ -0,0 +1,27 @@
+// import { Component } from "react";
+
+
+// class ClassOneComp extends Component{
+//     render(){
+//         return <div>
+//             <h1>Welcome to ClassOneComp</h1>
+//         </div>
+//     }
+
+    
+// }
+// export default ClassOneComp;
+
+// class ClassOneComp extends Component{
+// state={
+//     message:"Welcome to ClassOneComp"
+// }
+//  render(){
+//     return <div>
+//         <h1>{this.state.message}</h1>
+//     </div>
+//  }
+// }
+// export  default ClassOneComp ;
+
+
