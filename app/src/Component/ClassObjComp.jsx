3540eeec256a3873ead78503d55be5f84680f102
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-29 15:06:51 +0530

    React-js

---
+++ app/src/Component/ClassObjComp.jsx
@@ -0,0 +1,56 @@
+import { Component } from "react";
+
+
+class ClassObjComp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: "Welcome to ClassObjComp",
+
+            bike: {
+                bname: "Fz-x",
+                bprice: "160000",
+                bmilage: "45km",
+
+            },
+            bikes:[
+                "Fz-x",
+                "Rajdooth",
+                "yezdi",
+                "classic350",
+                "passion pro",
+
+            ]
+
+
+        }
+    }
+    render() {
+        const {message,bike,bikes}=this.state
+            
+        return <div>
+            <h1>{message}</h1>
+
+            <hr />
+             <ul>
+                <li>{this.state.bike.bname}</li>
+                <li>{this.state.bike.bprice}</li>
+                <li>{this.state.bike.bmilage}</li>
+            </ul>
+            <ul>
+                {Object.values(bike).map((val,i)=>{
+                    return<li key={i}>{val}</li>
+                })}
+            </ul>
+<ul>
+    {bikes.map((val,i)=>{
+        return <li key={i}>{val} </li>
+    })}
+</ul>
+
+        </div> 
+    }
+
+}
+
+export default ClassObjComp;
