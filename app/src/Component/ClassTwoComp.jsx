ade687348b570d3776b49b74a600549079ea121e
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-29 15:07:26 +0530

    React-js

---
+++ app/src/Component/ClassTwoComp.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+
+
+export default class ClassTwoComp extends Component{
+    render(){
+        return <div>
+            <h1>Welcome to ClassTwoComp</h1>
+        </div>
+    }
+
+};
+
+export class ClassThreeComp extends Component{
+    render(){
+        return <div>
+            <h2>Welcome to ClassThreeComp</h2>
+        </div>
+    }
+}
