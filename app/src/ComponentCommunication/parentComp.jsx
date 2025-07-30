d90d4f3c29c35e85f08a328df067ee5b87f084af
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-30 16:30:01 +0530

    React-js

--- app/src/ComponentCommunication/parentComp.jsx
+++ app/src/ComponentCommunication/parentComp.jsx
@@ -2,30 +2,30 @@ import { Component } from "react";
 import ChildComp from "./childComp";
 
 export default class ParentComp extends Component {
-  constructor() {
-    super();
-    // This is called the state in parent
-    this.state = {
-      message: "Parent Component text Is Called from Child Component !!",
+    constructor() {
+        super();
+        // This is called the state in parent
+        this.state = {
+            message: "Parent Component text Is Called from Child Component !!",
+        };
+    }
+
+    changeMsgInParent = () => {
+        this.setState({
+            message: "Text Changed in Parent When You clicked the Button",
+        });
     };
-  }
 
-  changeMsgInParent = () => {
-    this.setState({
-      message: "Text Changed in Parent When You clicked the Button",
-    });
-  };
+    render() {
+        return (
+            <div>
+                <h2>Parent Component</h2>
+                <button onClick={this.changeMsgInParent}>Change Message</button>
 
-  render() {
-    return (
-      <div>
-        <h2>Parent Component</h2>
-        <button onClick={this.changeMsgInParent}>Change Message</button>
-
-        <hr />
-        {/* Child component called from Parent Component */}
-        <ChildComp newMessage={this.state.message} />
-      </div>
-    );
-  }
+                <hr />
+                {/* Child component called from Parent Component */}
+                <ChildComp newMessage={this.state.message} />
+            </div>
+        );
+    }
 }
