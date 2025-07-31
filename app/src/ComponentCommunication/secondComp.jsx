71acda7e8bb2fd4aa838030d9af91ef0d0414f69
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-31 14:56:23 +0530

    React-js

---
+++ app/src/ComponentCommunication/secondComp.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+export default class SecondComp extends Component{
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+    render(){
+        return(<div>
+            <h2>{this.props.newmessage}</h2>
+        </div>)
+        
+    }
+}
