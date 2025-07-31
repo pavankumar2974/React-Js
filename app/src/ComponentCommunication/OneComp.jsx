de5b28ba2c85a5820fd3e93b2df2777429e3b1a9
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-07-31 15:57:11 +0530

    React-js

--- app/src/ComponentCommunication/OneComp.jsx
+++ app/src/ComponentCommunication/OneComp.jsx
@@ -3,26 +3,45 @@ import TwoComp from "./TwoComp";
 
 
 
-export default class OneComp extends Component{
-constructor(){
-    super();
-    this.state={
-        message:"Welcome to OneComp",
-    }
-}
-ChangeMsgInOne=()=>{
-    this.setState({
-        message:"The message is changed when clicked the button"
-    })
-}
-render(){
-    const{message}=this.state;
-    return(<div>
-        <TwoComp newmessage={message}/>
-        <button onClick={this.ChangeMsgInOne}>Change</button>
-    </div>
+export default class OneComp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: [
+                "pavan",
+                "21",
+                "pavan@gmail.com"
 
-    )
-}
+            ],
+        }
+    }
+    // ChangeMsgInOne=()=>{
+    //     this.setState({
+    //         message:{
+    //             name:"yaswant",
+    //             age:"20",
+    //             gmail:"yaswant@gmail.com"
+    //         }
+    //     })
+    // }
+
+    ChangeMsgInOne = () => {
+        this.setState({
+            message: [
+                "yaswant",
+                "20",
+                "yaswant@gmail.com"
+            ]
+        })
+    }
+    render() {
+        const { message } = this.state;
+        return (<div>
+            <TwoComp newmessage={message} />
+            <button onClick={this.ChangeMsgInOne}>Change</button>
+        </div>
+
+        )
+    }
 
 }
