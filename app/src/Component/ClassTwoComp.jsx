62b882b00fd550fc99e4a453b0fb7356d7979971
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-02 16:14:04 +0530

    React-js

--- app/src/Component/ClassTwoComp.jsx
+++ app/src/Component/ClassTwoComp.jsx
@@ -1,8 +1,8 @@
 import { Component } from "react";
 
 
-export default class ClassTwoComp extends Component{
-    render(){
+export default class ClassTwoComp extends Component {
+    render() {
         return <div>
             <h1>Welcome to ClassTwoComp</h1>
         </div>
@@ -10,8 +10,8 @@ export default class ClassTwoComp extends Component{
 
 };
 
-export class ClassThreeComp extends Component{
-    render(){
+export class ClassThreeComp extends Component {
+    render() {
         return <div>
             <h2>Welcome to ClassThreeComp</h2>
         </div>
