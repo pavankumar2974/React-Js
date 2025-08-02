bb447c0fecc9ed96a713710be5bb059e0fa8a11f
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-02 16:14:31 +0530

    React-js

--- app/src/Component/ToggleButton.jsx
+++ app/src/Component/ToggleButton.jsx
@@ -1,27 +1,27 @@
 import { Component } from "react";
 
 export default class ToggleButton extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      isVisible: false,
+        this.state = {
+            isVisible: false,
+        };
+    }
+
+    toggleParagraph = () => {
+        this.setState({ isVisible: !this.state.isVisible });
     };
-  }
 
-  toggleParagraph = () => {
-    this.setState({ isVisible: !this.state.isVisible });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.toggleParagraph}>
-          {/* {this.state.isVisible === true ? "show" : "Hide"} - Paragraph */}
-          {this.state.isVisible ? "Show" : "Hide"} Paragraph
-        </button>
-        {this.state.isVisible && <p>Welcome to Paragraph !!</p>}
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <button onClick={this.toggleParagraph}>
+                    {/* {this.state.isVisible === true ? "show" : "Hide"} - Paragraph */}
+                    {this.state.isVisible ? "Show" : "Hide"} Paragraph
+                </button>
+                {this.state.isVisible && <p>Welcome to Paragraph !!</p>}
+            </div>
+        );
+    }
 }
