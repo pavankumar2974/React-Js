b0a2046b9190d64d0088e07ec8c84c48ee773fb6
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-04 15:31:45 +0530

    React-js

--- app1/src/Component/ToggleButton.jsx
+++ app1/src/Component/ToggleButton.jsx
@@ -1,26 +1,54 @@
+// import { Component } from "react";
+
+
+// export default class ToggleButton extends Component{
+//     constructor(props){
+//         super(props)
+//         this.state={
+//             isvisble:true,
+//         }
+//     }
+//     toggleParagraph=()=>{
+//         this.setState({isvisble:this.state.isvisble});
+//     };
+
+//     render(){
+//         return(
+//             <div>
+//                 <button onClick={this.toggleParagraph}>
+//                     change 
+//                     {this.state.isvisble === true ? "show" : "Hide"} - Paragraph
+//                     {this.state.isvisble && <p>Welcome to Paragraph !!</p>}
+//                 </button>
+//             </div>
+//         )
+//     }
+// }
+
 import { Component } from "react";
 
+export default class ToggleButton extends Component {
+    constructor(props) {
+        super(props);
 
-export default class ToggleButton extends Component{
-    constructor(props){
-        super(props)
-        this.state={
-            isvisble:true,
-        }
+        this.state = {
+            isVisible: false,
+        };
     }
-    toggleParagraph=()=>{
-        this.setState({isvisble:this.state.isvisble});
+
+    toggleParagraph = () => {
+        this.setState({ isVisible: !this.state.isVisible });
     };
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <button onClick={this.toggleParagraph}>
-                    change 
-                    {this.state.isvisble === true ? "show" : "Hide"} - Paragraph
-                    {this.state.isvisble && <p>Welcome to Paragraph !!</p>}
+                    {/* {this.state.isVisible === true ? "show" : "Hide"} - Paragraph */}
+                    {this.state.isVisible ? "Show" : "Hide"} Paragraph
                 </button>
+                {this.state.isVisible && <p>Welcome to Paragraph !!</p>}
             </div>
-        )
+        );
     }
 }
