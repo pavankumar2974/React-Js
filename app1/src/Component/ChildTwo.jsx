1ae72239f825b5ccd5825f783e8b49faa5100d32
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-05 20:32:58 +0530

    react-js

--- app1/src/Component/ChildTwo.jsx
+++ app1/src/Component/ChildTwo.jsx
@@ -1,26 +1,26 @@
 import { Component } from "react";
 
 
-export default class ChildTwo extends Component{
+export default class ChildTwo extends Component {
     constructor(props) {
-    super(props);
+        super(props);
 
-    this.state = {
-      message: "Iam coming From Child !",
+        this.state = {
+            message: "Iam coming From Child !",
+        };
+    }
+
+    sendDataToParent = () => {
+        this.props.getInfo(this.state.message);
     };
-  }
 
-  sendDataToParent = () => {
-    this.props.getInfo(this.state.message);
-  };
-
-  render() {
-    console.log(this.props.getInfo);
-    return (
-      <div>
-        <h2>Iam from Child Component</h2>
-        <button onClick={this.sendDataToParent}>Send Data To Parent</button>
-      </div>
-    );
-  }
+    render() {
+        console.log(this.props.getInfo);
+        return (
+            <div>
+                <h2>Iam from Child Component</h2>
+                <button onClick={this.sendDataToParent}>Send Data To Parent</button>
+            </div>
+        );
+    }
 }
