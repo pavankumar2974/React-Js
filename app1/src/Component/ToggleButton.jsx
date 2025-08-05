d7c2dfb39b297a376512a2498db195ed6d7b0797
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-05 20:30:52 +0530

    react-js

--- app1/src/Component/ToggleButton.jsx
+++ app1/src/Component/ToggleButton.jsx
@@ -53,26 +53,57 @@
 //     }
 // }
 
-import { Component } from "react"
+// import { Component } from "react"
 
-export default class ToggleButton extends Component{
-constructor(props){
+// export default class ToggleButton extends Component{
+// constructor(props){
+//     super(props);
+
+//     this.state={
+//         isvisible:false,
+//     };
+// }
+// toggleParagraph=()=>{
+//     this.setState({isvisible:this.state.isvisible});
+// };
+
+// render(){
+//     return(
+//         <div>
+//             <button onClick={this.toggleParagraph}>{this.state.isvisible?"show":"hide"}Paragraph</button>
+//             {this.state.isvisible&&<p>Welcome to Paragraph!!</p>}
+//         </div>
+//     )
+// }
+// }
+
+
+
+
+import { Component } from "react";
+
+export default class ToggleButton extends Component {
+  constructor(props) {
     super(props);
 
-    this.state={
-        isvisible:false,
+    this.state = {
+      isVisible: false,
     };
-}
-toggleParagraph=()=>{
-    this.setState({isvisible:this.state.isvisible});
-};
+  }
 
-render(){
-    return(
-        <div>
-            <button onClick={this.toggleParagraph}>{this.state.isvisible?"show":"hide"}Paragraph</button>
-            {this.state.isvisible&&<p>Welcome to Paragraph!!</p>}
-        </div>
-    )
-}
+  toggleParagraph = () => {
+    this.setState({ isVisible: !this.state.isVisible });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggleParagraph}>
+          {/* {this.state.isVisible === true ? "show" : "Hide"} - Paragraph */}
+          {this.state.isVisible ? "Show" : "Hide"} Paragraph
+        </button>
+        {this.state.isVisible && <p>Welcome to Paragraph !!</p>}
+      </div>
+    );
+  }
 }
