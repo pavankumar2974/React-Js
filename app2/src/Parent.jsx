44f5f8507d779850fe0b1b839fb6bd3fddefa7f5
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-09 19:44:20 +0530

    react-js

---
+++ app2/src/Parent.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+
+
+
+export default class parent extends Component{
+    constructor(){
+        super();
+        this.state={
+            message:"",
+        };
+    }
+
+    receiveDataFromChild=(msg)=>{
+        console.log("DatamFrom Child",msg);
+        this.setState({message:msg});
+        
+    };
+
+    render(){
+        return(
+            <div>
+                <h2>{this.state.message}</h2>
+                
+            </div>
+        )
+    }
+}
