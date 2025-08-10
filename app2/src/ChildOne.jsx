6f02c3e322087f25c4769814c7e2e436bc130d22
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-10 19:04:27 +0530

    React-js

--- app2/src/ChildOne.jsx
+++ app2/src/ChildOne.jsx
@@ -2,26 +2,26 @@ import { Component } from "react";
 
 
 export default class ChildOne extends Component {
-constructor(props){
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state={
-        message:"I am Coming From Child",
+        this.state = {
+            message: "I am Coming From Child",
+        };
+    }
+
+    sendDataToparent = () => {
+        this.props.getInfo(this.state.message);
     };
-}
 
-sendDataToparent=()=>{
-    this.props.getInfo(this.state.message);
-};
-
-render(){
-    console.log(this.props.getInfo);
-    return(
-        <div>
-            <h2>Iam From Child Component</h2>
-            <button onClick={this.sendDataToparent}>Send Data to parent</button>
-        </div>
-    );
-}
+    render() {
+        console.log(this.props.getInfo);
+        return (
+            <div>
+                <h2>Iam From Child Component</h2>
+                <button onClick={this.sendDataToparent}>Send Data to parent</button>
+            </div>
+        );
+    }
 
 }
