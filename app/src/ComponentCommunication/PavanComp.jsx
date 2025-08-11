f11bcbc79cc56828659ca65cbad9a8889656b64d
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-11 15:35:17 +0530

    React-js

--- app/src/ComponentCommunication/PavanComp.jsx
+++ app/src/ComponentCommunication/PavanComp.jsx
@@ -3,25 +3,25 @@ import ChildComp from "./childComp";
 import KumarComp from "./kumarComp";
 
 
-export default class PavanComp extends Component{
-    constructor(){
-    super();
-    this.state={
-        message:"Pavan Component is called from kumar component"
-    }    
+export default class PavanComp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: "Pavan Component is called from kumar component"
+        }
     }
 
-    changeMsgInPavan =()=>{
+    changeMsgInPavan = () => {
         this.setState({
-            message:"Text is changed when you click the button"
+            message: "Text is changed when you click the button"
         })
     }
 
-    render(){
-        const {message}=this.state
-        return(
+    render() {
+        const { message } = this.state
+        return (
             <div>
-                <KumarComp newmessage={message}/>
+                <KumarComp newmessage={message} />
                 <button onClick={this.changeMsgInPavan}>Change</button>
             </div>
         )
