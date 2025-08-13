d4c2a74f52f5c84750a02d28604ed04d95935071
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-13 20:16:42 +0530

    React-js

---
+++ app3/src/Components/CompA.jsx
@@ -0,0 +1,19 @@
+
+
+import { Component } from "react";
+
+export default class CompA extends Component {
+  changeMsg = () => {
+    this.props.changeMsgComp();
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h2>Iam From Component A</h2>
+        <button onClick={this.changeMsg}>Change Comp B</button>
+      </div>
+    );
+  }
+}
