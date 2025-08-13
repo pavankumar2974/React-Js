346b26542b3af7f10518d8dea2ea9ed23951d2f9
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-13 20:23:41 +0530

    React-js

--- app3/src/Components/SibilingComp.jsx
+++ app3/src/Components/SibilingComp.jsx
@@ -5,25 +5,25 @@ import CompB from "./CompB";
 
 
 export default class SiblingComp extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      commonMessage: "Welcome To Common Message",
+        this.state = {
+            commonMessage: "Welcome To Common Message",
+        };
+    }
+
+    msgChangFunc = () => {
+        this.setState({ commonMessage: "I Got Changed From Comp A" });
     };
-  }
 
-  msgChangFunc = () => {
-    this.setState({ commonMessage: "I Got Changed From Comp A" });
-  };
-
-  render() {
-    return (
-      <div>
-        <CompA changeMsgComp={this.msgChangFunc} />
-        <hr />
-        <CompB msg={this.state.commonMessage} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <CompA changeMsgComp={this.msgChangFunc} />
+                <hr />
+                <CompB msg={this.state.commonMessage} />
+            </div>
+        );
+    }
 }
