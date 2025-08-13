52b8618a7a18e7791e4844c3c0ef6fee123ad95f
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-13 20:22:26 +0530

    React-js

--- app3/src/Components/CompA.jsx
+++ app3/src/Components/CompA.jsx
@@ -3,17 +3,17 @@
 import { Component } from "react";
 
 export default class CompA extends Component {
-  changeMsg = () => {
-    this.props.changeMsgComp();
-  };
+    changeMsg = () => {
+        this.props.changeMsgComp();
+    };
 
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <h2>Iam From Component A</h2>
-        <button onClick={this.changeMsg}>Change Comp B</button>
-      </div>
-    );
-  }
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <h2>Iam From Component A</h2>
+                <button onClick={this.changeMsg}>Change Comp B</button>
+            </div>
+        );
+    }
 }
