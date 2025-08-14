3f34f931fa8134bacaa7c2021970f6a561fc5cf7
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-14 19:40:17 +0530

    SiblingComponent

---
+++ app3/src/ComponentComunication/Sibling.jsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+import AComp from "./AComp";
+import BComp from "./BComp";
+
+
+
+export default class Sibling extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            CommonMessage:"Welcome to Common Message",
+        };
+    }
+
+    msgChangFunc=()=>{
+        this.setState({CommonMessage:"I got Changed From CompA"});
+    };
+
+    render(){
+        return(
+            <div>
+                <AComp changeMsgComp={this.msgChangFunc} />
+                <hr/>
+                <BComp msg={this.state.CommonMessage} />
+            </div>
+        );
+    }
+}
