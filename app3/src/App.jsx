e24d98b3e69711ef6938cb2e754bdc711c7ee955
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-14 19:41:47 +0530

    SiblingComponent

--- app3/src/App.jsx
+++ app3/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SiblingComp from './Components/SibilingComp'
+import Sibling from './ComponentComunication/Sibling'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
    <div>
     <SiblingComp/>
+    <Sibling/>
    </div>
   )
 }
