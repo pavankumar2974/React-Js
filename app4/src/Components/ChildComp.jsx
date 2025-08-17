2afd4103e51305b1ffa8774de8ffa4caa8317b61
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-17 13:49:11 +0530

    React-js

---
+++ app4/src/Components/ChildComp.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+
+export  default class ChildComp extends Component{
+render(){
+    return(
+        <div>
+           <h1>I am from Child Compomnent</h1> 
+<hr/>
+<h3>{this.props.newmessage}</h3>
+        </div>
+    )
+}
+    }
