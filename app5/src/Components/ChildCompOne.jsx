3f429244071303de98c2734f2b2ccd8bced15450
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-18 14:12:29 +0530

    ComponentCommunication

---
+++ app5/src/Components/ChildCompOne.jsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+
+export default class ChildCompOne extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            message:"I am Coming from child",
+        };
+    }
+
+    sendDataToParent=()=>{
+        this.props.getInfo(this.state.message);
+    };
+
+    render(){
+        // console.log(this.props.getInfo)
+        return(
+            <div>
+                {/* <h1>I am child Component</h1> */}
+                <button onClick={this.sendDataToParent}>Send data to parent</button>
+            </div>
+        )
+    }
+}
---
+++ app5/src/Components/ParentCompOne.jsx
@@ -0,0 +1,28 @@
+// T get the data from Child 
+
+import { Component } from "react";
+import ChildCompOne from "./ChildCompOne";   
+
+export default class ParentCompOne extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: "",
+        };
+    }
+
+    receiveDataFromChild = (msg) => {
+        // console.log("Data From Child:", msg);
+        this.setState({ message: msg });
+    };
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.message}</h2>
+                
+                <ChildCompOne getInfo={this.receiveDataFromChild} />
+            </div>
+        );
+    }
+}
