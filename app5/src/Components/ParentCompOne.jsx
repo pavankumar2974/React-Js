356e1431d70ed053f7c281ba4d7f148adb461469
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-18 14:17:50 +0530

    ComponentCommunication

--- app5/src/Components/ParentCompOne.jsx
+++ app5/src/Components/ParentCompOne.jsx
@@ -1,7 +1,7 @@
 // T get the data from Child 
 
 import { Component } from "react";
-import ChildCompOne from "./ChildCompOne";   
+import ChildCompOne from "./ChildCompOne";
 
 export default class ParentCompOne extends Component {
     constructor() {
@@ -20,7 +20,7 @@ export default class ParentCompOne extends Component {
         return (
             <div>
                 <h2>{this.state.message}</h2>
-                
+
                 <ChildCompOne getInfo={this.receiveDataFromChild} />
             </div>
         );
