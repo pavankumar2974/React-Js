e6c0997c82bb4ac85dfc2bec18ebbac932d83616
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-18 15:05:25 +0530

    React-js

--- app5/src/App.jsx
+++ app5/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ParentComp from './Components/ParentComp'
-import ParentCompOne from './Components/ParentCompOne'
+import ParentComp from './Components/Parent-child/ParentComp'
+import ParentCompOne from './Components/child-parent/ParentCompOne'
+import Sibling from './Components/SiblingComponent/Sibling'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,8 @@ function App() {
   return (
     <div>
       {/* <ParentComp/> */}
-      <ParentCompOne/>
+      {/* <ParentCompOne/> */}
+      <Sibling />
     </div>
   )
 }
