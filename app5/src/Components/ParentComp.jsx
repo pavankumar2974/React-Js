7b009d2d6f7a783c0138a0294314d9e16972bc68
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-18 14:12:12 +0530

    ComponentCommunication

---
+++ app5/src/Components/ParentComp.jsx
@@ -0,0 +1,22 @@
+// Calling a parent state  from child Component
+
+import { Component } from "react";
+import ChildComp from "./ChildComp";
+
+
+export default class ParentComp extends Component{
+    constructor(){
+        super();
+        this.state={
+            message:"Welcome to parent Component called from child Component ",
+        }
+    }
+    render(){
+        return(
+            <div>
+                <ChildComp newmessage={this.state.message} />
+            </div>
+        )
+    }
+}
+
