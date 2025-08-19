6bd63219a2250904d046c17e58f152b820461d0f
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-19 15:32:17 +0530

    React-js

---
+++ app6/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Users from './components/Users'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Users />
+       </div>
+    </>
+  )
+}
+
+export default App
