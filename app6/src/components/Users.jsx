bf5adb5f6e34c9fa7f7395ab6c40061f6484215c
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-19 15:31:37 +0530

    React-js

---
+++ app6/src/components/Users.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+import { use } from "react";
+
+const Users = () => {
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        phone: "",
+    })
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+
+        setUser((prev)=>({
+            ...prev, [name]: value
+        }))
+    } 
+
+    const handleSubmit = (e) => {
+        e.preventDefault
+        console.log(user)
+    }
+
+    return (
+        <div>
+            <label htmlFor="">Fname</label>
+            <input type="text" name="name" value={user.name} onChange={handleChange} /><br />
+            <label htmlFor="">Email</label>
+            <input type="email" name="email"value={user.email} onChange={handleChange}  /><br />
+            <label htmlFor="">Phone</label>
+            <input type="number" name="phone"value={user.phone} onChange={handleChange}  /><br />
+            <button type="submit" onClick={handleSubmit} >Submit</button>
+        </div>
+    )
+}
+
+export default Users
