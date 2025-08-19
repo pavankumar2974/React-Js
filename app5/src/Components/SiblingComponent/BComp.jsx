101f7dd9db37cb076f06dc6f2e0f5d87d53ba305
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-19 12:31:37 +0530

    SiblingComponentCommunication

--- app5/src/Components/SiblingComponent/BComp.jsx
+++ app5/src/Components/SiblingComponent/BComp.jsx
@@ -5,7 +5,8 @@ import { Component } from "react";
 export default class BComp extends Component{
     render(){
         return(<div>
-            <h1>I am from B component called from Sibling Compomnent</h1>
+            {/* <h1>I am from B component called from Sibling Compomnent</h1> */}
+        <p>{this.props.msg}</p>
         </div>)
     }
 }
