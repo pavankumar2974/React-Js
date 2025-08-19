d6124f5562bc3daa716aa66403759e89a418f65c
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-19 12:52:45 +0530

    SiblingComponentCommunication

--- app5/src/Components/SiblingComponent/SiblingComp.jsx
+++ app5/src/Components/SiblingComponent/SiblingComp.jsx
@@ -5,25 +5,25 @@ import BOne from "./BOne";
 
 
 
-export default class SiblingComp extends Component{
-    constructor(props){
+export default class SiblingComp extends Component {
+    constructor(props) {
         super(props);
 
-        this.state={
-            CommonMessage:"Welcome to Common Message",
+        this.state = {
+            CommonMessage: "Welcome to Common Message",
         };
     }
 
-    msgChangfunc=()=>{
-        this.setState({CommonMessage:"I am  a changed message from CompA"});
+    msgChangfunc = () => {
+        this.setState({ CommonMessage: "I am  a changed message from CompA" });
     };
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-              <AOne ChangeMsgComp={this.msgChangfunc} />
-              <hr/>
-              <BOne msg={this.state.CommonMessage} /> 
+                <AOne ChangeMsgComp={this.msgChangfunc} />
+                <hr />
+                <BOne msg={this.state.CommonMessage} />
             </div>
         )
     }
