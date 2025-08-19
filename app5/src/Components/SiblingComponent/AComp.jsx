697d95ee5bd065ce1839395caaf2ebad353aa0bf
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-19 12:31:15 +0530

    SiblingComponentCommunication

--- app5/src/Components/SiblingComponent/AComp.jsx
+++ app5/src/Components/SiblingComponent/AComp.jsx
@@ -2,10 +2,14 @@ import { Component } from "react";
 
 
 export default class AComp extends Component{
+    changeMsg=()=>{
+        this.props.changeMsgComp();
+    }
     render(){
         return(
             <div>
-                <h1>I am from A Component called by Sibiling Component</h1>
+                {/* <h1>I am from A Component called by Sibiling Component</h1> */}
+            <button onClick={this.changeMsg}>change Comp B</button>
             </div>
         )
     }
