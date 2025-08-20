36cffbc08b02488a475fb45be01707b89ac70bf0
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-20 21:31:03 +0530

    React-js

--- app6/src/components/Users.jsx
+++ app6/src/components/Users.jsx
@@ -9,19 +9,19 @@ const Users = () => {
         phone: "",
     })
 
-    const [Users , setUsers]=useState([])
+    const [Users, setUsers] = useState([])
 
     const handleChange = (e) => {
-        const {name, value} = e.target
+        const { name, value } = e.target
 
-        setUser((prev)=>({
+        setUser((prev) => ({
             ...prev, [name]: value
         }))
-    } 
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault
-        setUsers((prev) => [...prev. user])
+        setUsers((prev) => [...prev.user])
         console.log(Users)
     }
 
@@ -30,9 +30,9 @@ const Users = () => {
             <label htmlFor="">Fname</label>
             <input type="text" name="name" value={user.name} onChange={handleChange} /><br />
             <label htmlFor="">Email</label>
-            <input type="email" name="email"value={user.email} onChange={handleChange}  /><br />
+            <input type="email" name="email" value={user.email} onChange={handleChange} /><br />
             <label htmlFor="">Phone</label>
-            <input type="number" name="phone"value={user.phone} onChange={handleChange}  /><br />
+            <input type="number" name="phone" value={user.phone} onChange={handleChange} /><br />
             <button type="submit" onClick={handleSubmit} >Submit</button>
         </div>
     )
