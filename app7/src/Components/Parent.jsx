b5f7bf107c023a4300d3c453c9235c84753f7e3d
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-25 21:15:30 +0530

    React-js

--- app7/src/Components/Parent.jsx
+++ app7/src/Components/Parent.jsx
@@ -2,16 +2,16 @@ import { Component } from "react";
 import Child from "./Child";
 
 
-export default class Parent extends Component{
-    constructor(){
+export default class Parent extends Component {
+    constructor() {
         super();
-        this.state={
-            message:"Hi I am from Parent Component called by child component by using props in child and assgisning the state to a varible"
+        this.state = {
+            message: "Hi I am from Parent Component called by child component by using props in child and assgisning the state to a varible"
         }
     }
 
-    render(){
-        return(<div>
+    render() {
+        return (<div>
             <Child newmessage={this.state.message} />
         </div>)
     }
