3d7e99e62da1b65cc2e3d0f2506fecc39873af23
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-25 21:14:48 +0530

    React-js

--- app7/src/Components/AComp.jsx
+++ app7/src/Components/AComp.jsx
@@ -1,9 +1,9 @@
 import { Component } from "react";
 
 
-export default class AComp extends Component{
-    render(){
-        return(
+export default class AComp extends Component {
+    render() {
+        return (
             <div>
                 <h1>I am from AComp</h1>
             </div>
