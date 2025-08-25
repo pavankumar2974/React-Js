52d0212e2ea1e1650f8d799aa13da4f65b323ea2
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-25 13:15:18 +0530

    React-js

---
+++ app7/src/Components/AComp.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+
+export default class AComp extends Component{
+    render(){
+        return(
+            <div>
+                <h1>I am from AComp</h1>
+            </div>
+        )
+    }
+}
