4870132ce8353d715884afc18ae3741d5256e98d
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-25 21:15:21 +0530

    React-js

--- app7/src/Components/ChildComp.jsx
+++ app7/src/Components/ChildComp.jsx
@@ -1,18 +1,18 @@
 import { Component } from "react";
 
 
-export default class ChildComp extends Component{
-    constructor(props){
+export default class ChildComp extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            message:" i am from child Component  called by parent Component using props and function"
+        this.state = {
+            message: " i am from child Component  called by parent Component using props and function"
         }
     }
-    senddatatoparent=()=>{
+    senddatatoparent = () => {
         this.props.getInfo(this.state.message)
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <button onClick={this.senddatatoparent}>Change</button>
             </div>
