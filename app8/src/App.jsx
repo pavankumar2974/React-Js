6482eadceab64183b8234320aad1b9aecdaaa784
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-26 15:15:44 +0530

    React-js

--- app8/src/App.jsx
+++ app8/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Parent from './Components/Parent'
+import Sibling from './Components/Sibling/Sibling'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <div>
-<Parent/>
+{/* <Parent/> */}
+<Sibling />
 
  </div>
     </>
