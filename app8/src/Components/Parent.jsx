957e3ca4840e55623ecd96caddc74613873ded78
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-26 14:26:31 +0530

    React-js

--- app8/src/Components/Parent.jsx
+++ app8/src/Components/Parent.jsx
@@ -2,19 +2,19 @@ import { Component } from "react";
 import Child from "./Child";
 
 
-export default class Parent extends Component{
-    constructor(){
+export default class Parent extends Component {
+    constructor() {
         super();
-        this.state={
-            message:""
+        this.state = {
+            message: ""
         }
     }
-        receiveDataFromChild=(Msg)=>{
-            this.setState({message:Msg})
-        }
-    
-    render(){
-        return(
+    receiveDataFromChild = (Msg) => {
+        this.setState({ message: Msg })
+    }
+
+    render() {
+        return (
             <div>
                 <h2>{this.state.message}</h2>
                 <Child getInfo={this.receiveDataFromChild} />
