0b1e1285d9b9b77039d20be281bf9d3416f118d2
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-28 20:00:59 +0530

    ComponentCommunication

---
+++ app9/src/Components/Parent.jsx
@@ -0,0 +1,20 @@
+//Calling a Parent state in child
+import { Component } from "react";
+import Child from "./Child";
+
+
+export default class Parent extends Component{
+    constructor(){
+        super();
+        this.state={
+            message:"Hi I am from Parent Component"
+        }
+    }
+    render(){
+        return(
+            <div>
+                <Child newmessage={this.state.message} />
+            </div>
+        )
+    }
+}
