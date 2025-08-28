8558ba9a8dd683593b6d187d1df9a8e38dfbf9b5
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-28 20:14:00 +0530

    ComponentCommunication

---
+++ app9/src/Components/ChildComp.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+
+export default class ChildComp extends Component{
+    constructor(props){
+        super(props);
+        this.state={message:"Hi I am from child state called from parent"}
+    }
+    sendDataToParent=()=>{
+        this.props.getInfo(this.state.message);
+    }
+    render(){
+        return(<div>
+            <h2>Click the below button to get the message</h2>
+            <button onClick={this.sendDataToParent}>Send</button>
+        </div>)
+    }
+}
