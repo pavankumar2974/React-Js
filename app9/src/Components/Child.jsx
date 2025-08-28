5e2bcede8f03d1f4125e51690ad73ad23b9c1fd3
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-28 20:30:19 +0530

    ComponentCommunication

--- app9/src/Components/Child.jsx
+++ app9/src/Components/Child.jsx
@@ -1,9 +1,9 @@
 import { Component } from "react";
 
 
-export default class Child extends Component{
-    render(){
-        return(<div>
+export default class Child extends Component {
+    render() {
+        return (<div>
             <h1>{this.props.newmessage}</h1>
         </div>)
     }
