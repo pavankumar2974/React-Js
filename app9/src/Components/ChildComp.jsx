876da3e1a223706f6a57e7b296ea3a11832657b3
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-28 20:30:31 +0530

    ComponentCommunication

--- app9/src/Components/ChildComp.jsx
+++ app9/src/Components/ChildComp.jsx
@@ -1,16 +1,16 @@
 import { Component } from "react";
 
 
-export default class ChildComp extends Component{
-    constructor(props){
+export default class ChildComp extends Component {
+    constructor(props) {
         super(props);
-        this.state={message:"Hi I am from child state called from parent"}
+        this.state = { message: "Hi I am from child state called from parent" }
     }
-    sendDataToParent=()=>{
+    sendDataToParent = () => {
         this.props.getInfo(this.state.message);
     }
-    render(){
-        return(<div>
+    render() {
+        return (<div>
             <h2>Click the below button to get the message</h2>
             <button onClick={this.sendDataToParent}>Send</button>
         </div>)
