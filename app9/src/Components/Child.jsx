339c4876677a06fde0fcfb1a42a1ad80281b50e8
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-28 20:00:43 +0530

    ComponentCommunication

---
+++ app9/src/Components/Child.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+
+export default class Child extends Component{
+    render(){
+        return(<div>
+            <h1>{this.props.newmessage}</h1>
+        </div>)
+    }
+}
