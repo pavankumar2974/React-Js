a5037a4c287f54cfed16ea25ec23d20d199d232b
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-28 20:13:48 +0530

    ComponentCommunication

--- app9/src/App.jsx
+++ app9/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Parent from './Components/Parent'
+import ParentComp from './Components/ParentComp'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,8 @@ function App() {
   return (
     <>
       <div>
-        <Parent />
+        {/* <Parent /> */}
+        <ParentComp />
        </div>
     </>
   )
