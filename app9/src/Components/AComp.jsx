3bbbad64f99b9bc8c85ed2c4bc908ec876f5e009
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-29 16:05:30 +0530

    ComponentCommunication

--- app9/src/Components/AComp.jsx
+++ app9/src/Components/AComp.jsx
@@ -1,17 +1,17 @@
 import { Component } from "react";
 
 export default class AComp extends Component {
-  changeMsg = () => {
-    this.props.changeMsgComp();
-  };
+    changeMsg = () => {
+        this.props.changeMsgComp();
+    };
 
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <h2>Iam From Component A</h2>
-        <button onClick={this.changeMsg}>Change Comp B</button>
-      </div>
-    );
-  }
+    render() {
+        console.log(this.props);
+        return (
+            <div>
+                <h2>Iam From Component A</h2>
+                <button onClick={this.changeMsg}>Change Comp B</button>
+            </div>
+        );
+    }
 }
