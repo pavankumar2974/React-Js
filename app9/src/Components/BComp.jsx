0a7c87e0e0388087829682596e04567a45620a8a
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-08-29 16:05:40 +0530

    ComponentCommunication

--- app9/src/Components/BComp.jsx
+++ app9/src/Components/BComp.jsx
@@ -1,12 +1,12 @@
 import { Component } from "react";
 
 export default class BComp extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Iam From Component B</h2>
-        <p>{this.props.msg}</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h2>Iam From Component B</h2>
+                <p>{this.props.msg}</p>
+            </div>
+        );
+    }
 }
