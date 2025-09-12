617af02f5fcc768532709ab56bd8b39d21a910f2
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-12 21:23:31 +0530

    Crud

--- app11/src/CrudComp/Crudvalid.jsx
+++ app11/src/CrudComp/Crudvalid.jsx
@@ -1,133 +1,133 @@
 import { Component } from "react";
 
 export default class Crudvalid extends Component {
-  state = {
-    user: {
-      Name: "",
-      id: "",
-      email: "",
-    },
-    users: [],
-    editIndex: null,
-  };
-
-  handleChange = (e) => {
-    const inputName = e.target.name;
-    const userEnteredValue = e.target.value;
-    const newUser = { ...this.state.user };
-    newUser[inputName] = userEnteredValue;
-    this.setState({ user: newUser });
-    console.log(newUser);
-  };
-
-  handleSubmit = () => {
-    const newUsers = [...this.state.users];
-    newUsers.push(this.state.user);
-    this.setState({ users: newUsers }); 
-    this.handleReset();
-  };
-
-  handleReset = () => {
-    const resetUser = {
-      Name: "",
-      id: "",
-      email: "",
+    state = {
+        user: {
+            Name: "",
+            id: "",
+            email: "",
+        },
+        users: [],
+        editIndex: null,
     };
-    this.setState({ user: resetUser });
-  };
 
-  handleEdit = (usr, i) => {
-    this.setState({ user: usr, editIndex: i });
-  };
+    handleChange = (e) => {
+        const inputName = e.target.name;
+        const userEnteredValue = e.target.value;
+        const newUser = { ...this.state.user };
+        newUser[inputName] = userEnteredValue;
+        this.setState({ user: newUser });
+        console.log(newUser);
+    };
 
-  handleDelete = (usr) => {  
-    const newUsers = this.state.users.filter((del) => {
-      return del.email !== usr.email;
-    });
-    this.setState({ users: newUsers });
-  };
+    handleSubmit = () => {
+        const newUsers = [...this.state.users];
+        newUsers.push(this.state.user);
+        this.setState({ users: newUsers });
+        this.handleReset();
+    };
 
-  handleUpdate = () => {
-    const newUsers = [...this.state.users];
-    newUsers[this.state.editIndex] = this.state.user;
-    this.setState({ users: newUsers, editIndex: null });
-    this.handleReset();
-  };
+    handleReset = () => {
+        const resetUser = {
+            Name: "",
+            id: "",
+            email: "",
+        };
+        this.setState({ user: resetUser });
+    };
 
-  render() {
-    const { Name, id, email } = this.state.user;
+    handleEdit = (usr, i) => {
+        this.setState({ user: usr, editIndex: i });
+    };
 
-    return (
-      <div>
-        <form>
-          <label htmlFor="Name">Enter Name</label>
-          <input
-            type="text"
-            name="Name"
-            value={Name}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="id">Enter ID</label>
-          <input
-            type="text"
-            name="id"
-            value={id}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="email">Enter Email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <br />
-          {this.state.editIndex === null ? (
-            <input
-              type="button"
-              value="Submit"           
-              onClick={this.handleSubmit}
-            />
-          ) : (
-            <input
-              type="button"
-              value="Update"            
-              onClick={this.handleUpdate}
-            />
-          )}
-        </form>
+    handleDelete = (usr) => {
+        const newUsers = this.state.users.filter((del) => {
+            return del.email !== usr.email;
+        });
+        this.setState({ users: newUsers });
+    };
 
-        <table border={2}>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>ID</th>
-              <th>Email</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.users.map((usr, i) => {
-              return (
-                <tr key={i}>
-                  <td>{usr.Name}</td>
-                  <td>{usr.id}</td>
-                  <td>{usr.email}</td>
-                  <td>
-                    <button onClick={() => this.handleEdit(usr, i)}>Edit</button>
-                  </td>
-                  <td>
-                    <button onClick={() => this.handleDelete(usr)}>Delete</button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+    handleUpdate = () => {
+        const newUsers = [...this.state.users];
+        newUsers[this.state.editIndex] = this.state.user;
+        this.setState({ users: newUsers, editIndex: null });
+        this.handleReset();
+    };
+
+    render() {
+        const { Name, id, email } = this.state.user;
+
+        return (
+            <div>
+                <form>
+                    <label htmlFor="Name">Enter Name</label>
+                    <input
+                        type="text"
+                        name="Name"
+                        value={Name}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="id">Enter ID</label>
+                    <input
+                        type="text"
+                        name="id"
+                        value={id}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="email">Enter Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={this.handleChange}
+                    />
+                    <br />
+                    {this.state.editIndex === null ? (
+                        <input
+                            type="button"
+                            value="Submit"
+                            onClick={this.handleSubmit}
+                        />
+                    ) : (
+                        <input
+                            type="button"
+                            value="Update"
+                            onClick={this.handleUpdate}
+                        />
+                    )}
+                </form>
+
+                <table border={2}>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>ID</th>
+                            <th>Email</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.users.map((usr, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td>{usr.Name}</td>
+                                    <td>{usr.id}</td>
+                                    <td>{usr.email}</td>
+                                    <td>
+                                        <button onClick={() => this.handleEdit(usr, i)}>Edit</button>
+                                    </td>
+                                    <td>
+                                        <button onClick={() => this.handleDelete(usr)}>Delete</button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
