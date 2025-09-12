f34d358b10944a9f9b7bcbb1e1fdb36e91d32b99
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-12 21:23:20 +0530

    Crud

--- app11/src/CrudComp/CrudThree.jsx
+++ app11/src/CrudComp/CrudThree.jsx
@@ -1,67 +1,67 @@
 import { Component } from "react";
 
 
-export default class CrudThree extends Component{
-    state={
-        user:{
-            Name:"",
-            age:"",
+export default class CrudThree extends Component {
+    state = {
+        user: {
+            Name: "",
+            age: "",
         },
 
-        users:[],
-        editIndex:null,
-       
+        users: [],
+        editIndex: null,
+
     };
-    handleChange=(e)=>{
-        const inputName=e.target.name;
-        const UserEnteredValue=e.target.value;
-        const newUser ={...this.state.user};
-        newUser[inputName]=UserEnteredValue;
-        this.setState({user:newUser})
+    handleChange = (e) => {
+        const inputName = e.target.name;
+        const UserEnteredValue = e.target.value;
+        const newUser = { ...this.state.user };
+        newUser[inputName] = UserEnteredValue;
+        this.setState({ user: newUser })
         // console.log(newUser);
     }
-    handleSubmit=()=>{
-        const newUsers=[...this.state.users];
+    handleSubmit = () => {
+        const newUsers = [...this.state.users];
         newUsers.push(this.state.user);
-        this.setState({users:newUsers});
+        this.setState({ users: newUsers });
         this.handleReset();
     }
-    handleReset=()=>{
-        const resetUser={
-            Name:"",
-            age:"",
+    handleReset = () => {
+        const resetUser = {
+            Name: "",
+            age: "",
         };
-        this.setState({user:resetUser});
+        this.setState({ user: resetUser });
     };
-    handleEdit=(usr,i)=>{
-        this.setState({user:usr,editIndex:i});
+    handleEdit = (usr, i) => {
+        this.setState({ user: usr, editIndex: i });
     };
-    handleDelete=(usr)=>{
-        const newUsers=this.state.users.filter((del)=>{
-            return del.Name!==usr.Name;
+    handleDelete = (usr) => {
+        const newUsers = this.state.users.filter((del) => {
+            return del.Name !== usr.Name;
         });
-        this.setState({users:newUsers});
+        this.setState({ users: newUsers });
     };
-    handleUpdate=()=>{
-        const newUsers=[...this.state.users];
-        newUsers[this.state.editIndex]=this.state.user;
-        this.setState({users:newUsers,editIndex:null});
+    handleUpdate = () => {
+        const newUsers = [...this.state.users];
+        newUsers[this.state.editIndex] = this.state.user;
+        this.setState({ users: newUsers, editIndex: null });
         this.handleReset();
     }
-    render(){
-        const{Name,age}=this.state.user;
-        return(
+    render() {
+        const { Name, age } = this.state.user;
+        return (
             <div>
                 <form>
                     <label htmlFor="Name" >Enter Name</label>
-                    <input type="text" name="Name" value={Name} onChange={this.handleChange}/>
-                    <br/>
+                    <input type="text" name="Name" value={Name} onChange={this.handleChange} />
+                    <br />
                     <label htmlFor="age" >Enter Age</label>
                     <input type="number" name="age" value={age} onChange={this.handleChange} />
-                    <br/>
-                    {this.state.editIndex===null?(
+                    <br />
+                    {this.state.editIndex === null ? (
                         <input type="button" value="submit" onClick={this.handleSubmit} />
-                    ):(
+                    ) : (
                         <input type="button" value="Update" onClick={this.handleUpdate} />
                     )}
                 </form>
@@ -75,16 +75,16 @@ export default class CrudThree extends Component{
                         </tr>
                     </thead>
                     <tbody>
-                        {this.state.users.map((usr,i)=>{
-                            return(
+                        {this.state.users.map((usr, i) => {
+                            return (
                                 <tr key={i}>
                                     <td>{usr.Name}</td>
                                     <td>{usr.age}</td>
-                                    <td><button onClick={()=>{
-                                        this.handleEdit(usr,i);
+                                    <td><button onClick={() => {
+                                        this.handleEdit(usr, i);
                                     }}>Edit</button></td>
-                                    <td><button onClick={()=>{
-                                        this.handleDelete(usr,i);
+                                    <td><button onClick={() => {
+                                        this.handleDelete(usr, i);
                                     }}>Delete</button></td>
                                 </tr>
                             )
