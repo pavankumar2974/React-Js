514f27685c1e8b90ef68d16b6c4d022511aec3b5
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-16 10:59:11 +0530

    lifecycleHooks

--- app13/src/App.jsx
+++ app13/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProductAdminPanel from './Components/ProductAdminPanel'
+import LifeCycleHooksOne from './Components/LifeCycleHooksOne'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <ProductAdminPanel />
+     <LifeCycleHooksOne/>   
       </div>
     </>
   )
