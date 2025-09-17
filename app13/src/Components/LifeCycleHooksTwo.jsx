708ddb0a24e24473b8db473b242f7ae854d863a8
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-17 21:00:44 +0530

    lifecyclehooks

---
+++ app13/src/Components/LifeCycleHooksTwo.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+
+
+export default class LifeCycleHooksTwo extends Component {
+    constructor() {
+        super();
+        console.log("Welcome to Constructor");
+        this.state = {
+            message: "",
+        };
+    }
+    componentDidMount() {
+        console.log("I am from componentDidMount");
+    }
+
+    render() {
+        console.log("I am from render Method");
+        return <div>LifeCycleHooksTwo
+           
+        </div>;
+    }
+    static getDerivedStateFromProps() {
+        console.log("Iam from getDerivedStateFromProps");
+        return null;
+    }
+
+
+}
