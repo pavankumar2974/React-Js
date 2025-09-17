e4fb9518c6b77f61a1dd53050d0ed949b326e000
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-17 21:00:53 +0530

    lifecyclehooks

--- app13/src/App.jsx
+++ app13/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LifeCycleHooksOne from './Components/LifeCycleHooksOne'
+import LifeCycleHooksTwo from './Components/LifeCycleHooksTwo'
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
   return (
     <>
       <div>
-        <LifeCycleHooksOne />
+       {/* <LifeCycleHooksOne /> */}
+        <LifeCycleHooksTwo/>
       </div>
     </>
   )
