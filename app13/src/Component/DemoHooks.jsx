173ae1fffb814362b9b703443991402cc77c5ab0
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-18 14:02:53 +0530

    lifeCycleHooks

--- app13/src/Component/DemoHooks.jsx
+++ app13/src/Component/DemoHooks.jsx
@@ -2,16 +2,30 @@ import { Component } from "react";
 
 
 export default class DemoHooks extends Component{
-   constructor(){
-super();
+   constructor(props){
+super(props);
 this.state={
+    message:"hello",
 users:[],
-}
+};
    }
    
+   static getDerivedStateFromProps(props,state){
+    console.log(props,state);
+    if(props.age <=18){
+        return{message:"you are a Teenager"};
+    }else if(props.age>18 && props.age <=58){
+        return{message:"you are an adult"};
+    }else{
+        return{message:"you are a senior citizen"};
+    }
+    return null;
+   }
+
   render(){
     return(
         <div>
+            <h2>{this.state.message}</h2>
             <ul>{this.state.users.map((usr,i)=>{
                 return<li key={i}>{usr.id},{usr.name},{usr.email}</li>
             })}</ul>
