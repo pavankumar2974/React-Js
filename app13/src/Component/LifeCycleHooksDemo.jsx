084b3685a76e9a01cb8b3df4e04a9ff97a326d64
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-19 20:20:13 +0530

    Hooks

--- app13/src/Component/LifeCycleHooksDemo.jsx
+++ app13/src/Component/LifeCycleHooksDemo.jsx
@@ -1,41 +1,41 @@
 import { Component, PureComponent } from "react";
 
 
-export default class LifeCycleHooksDemo extends Component{
-    constructor(props){
+export default class LifeCycleHooksDemo extends Component {
+    constructor(props) {
         super(props);
 
-        this.state={
-            message:"Hello I am from state Message",
-            users:[],
+        this.state = {
+            message: "Hello I am from state Message",
+            users: [],
         };
     }
-    
+
     // static getDerivedFromProps(props,state){
     //     console.log(props,state);
     // }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <h1>{this.state.message}</h1>
                 <ul>
-                    {this.state.users.map((usr,i)=>{
+                    {this.state.users.map((usr, i) => {
                         return <li key={i}>{usr.name},{usr.email},{usr.id},{usr.username}</li>;
                     })}
                 </ul>
             </div>
         )
     }
-    componentDidMount(){
+    componentDidMount() {
         fetch("https://jsonplaceholder.typicode.com/users")
-        .then((rawData)=>{
-            return rawData.json();
-        })
-        .then((response)=>{
-            console.log(response);
-            this.setState({users:response});
-        });
-        
+            .then((rawData) => {
+                return rawData.json();
+            })
+            .then((response) => {
+                console.log(response);
+                this.setState({ users: response });
+            });
+
     }
 }
