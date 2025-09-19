62dfb99355b88eb0dc0f8d1c98f51756a84443a4
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-19 20:20:03 +0530

    Hooks

--- app13/src/Component/DemoHooks.jsx
+++ app13/src/Component/DemoHooks.jsx
@@ -1,45 +1,45 @@
 import { Component } from "react";
 
 
-export default class DemoHooks extends Component{
-   constructor(props){
-super(props);
-this.state={
-    message:"hello",
-users:[],
-};
-   }
-   
-   static getDerivedStateFromProps(props,state){
-    console.log(props,state);
-    if(props.age <=18){
-        return{message:"you are a Teenager"};
-    }else if(props.age>18 && props.age <=58){
-        return{message:"you are an adult"};
-    }else{
-        return{message:"you are a senior citizen"};
+export default class DemoHooks extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "hello",
+            users: [],
+        };
     }
-    return null;
-   }
 
-  render(){
-    return(
-        <div>
-            <h2>{this.state.message}</h2>
-            <ul>{this.state.users.map((usr,i)=>{
-                return<li key={i}>{usr.id},{usr.name},{usr.email}</li>
-            })}</ul>
-        </div>
-    )
-  }
-    componentDidMount(){
+    static getDerivedStateFromProps(props, state) {
+        console.log(props, state);
+        if (props.age <= 18) {
+            return { message: "you are a Teenager" };
+        } else if (props.age > 18 && props.age <= 58) {
+            return { message: "you are an adult" };
+        } else {
+            return { message: "you are a senior citizen" };
+        }
+        return null;
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.message}</h2>
+                <ul>{this.state.users.map((usr, i) => {
+                    return <li key={i}>{usr.id},{usr.name},{usr.email}</li>
+                })}</ul>
+            </div>
+        )
+    }
+    componentDidMount() {
         fetch("https://jsonplaceholder.typicode.com/users")
-        .then((rawData)=>{
-            return rawData.json();
-        })
-        .then((response)=>{
-            console.log(response);
-            this.setState({users:response});        
-        });
+            .then((rawData) => {
+                return rawData.json();
+            })
+            .then((response) => {
+                console.log(response);
+                this.setState({ users: response });
+            });
     }
 }
