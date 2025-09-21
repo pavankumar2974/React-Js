501a59c7aa0753ae39f7a31ad89d856393819214
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-21 20:00:36 +0530

    ReactHooks

--- app14/src/Components/LifeCycleDemo.jsx
+++ app14/src/Components/LifeCycleDemo.jsx
@@ -3,19 +3,19 @@ import Child from "./Child";
 
 
 
-export default class LifeCycleDemo extends Component{
-    constructor(){
+export default class LifeCycleDemo extends Component {
+    constructor() {
         super();
-        this.state={
-            count:10,
+        this.state = {
+            count: 10,
         };
     }
 
-    incrementCount=()=>{
-        this.setState({count:this.state.count+10});
+    incrementCount = () => {
+        this.setState({ count: this.state.count + 10 });
     };
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <button onClick={this.incrementCount}>Increment</button>
                 <Child count={this.state.count} />
