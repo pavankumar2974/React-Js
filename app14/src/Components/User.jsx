acd3e70d304b9299ffba869091a2996a925b757b
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-21 19:59:14 +0530

    ReactHooks

---
+++ app14/src/Components/User.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+
+export default class User extends Component{
+    render(){
+        return(
+            <div>
+                <img src="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg" alt="" />
+
+            </div>
+        );
+    }
+    componentWillUnmount(){
+        alert("since you reached the limit, The Image will be removed");
+    }
+}
