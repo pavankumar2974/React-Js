d3aa26b31343e3246fa4136e44bfcbd9df76deca
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-22 17:16:50 +0530

    Hooks

--- app14/src/Components/LifeCycleDemo.jsx
+++ app14/src/Components/LifeCycleDemo.jsx
@@ -11,14 +11,26 @@ export default class LifeCycleDemo extends Component {
         };
     }
 
-    incrementCount = () => {
+    incrementCount = () => { 
         this.setState({ count: this.state.count + 10 });
+    };
+    decrementCount=()=>{
+        this.setState({count:this.state.count - 10 });
+    };
+    multipleCount=()=>{
+        this.setState({count:this.state.count*2});
+    };
+    divide=()=>{
+        this.setState({count:this.state.count/2});
     };
     render() {
         return (
             <div>
-                <button onClick={this.incrementCount}>Increment</button>
                 <Child count={this.state.count} />
+                <button onClick={this.incrementCount}>Increment</button>
+                <button onClick={this.decrementCount}>Decrement</button>
+                <button onClick={this.multipleCount}>Multiple</button>
+                <button onClick={this.divide}>Divide</button>
             </div>
         )
     }
