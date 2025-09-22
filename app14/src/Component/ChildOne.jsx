c506b57360923552c07b29feab64330bddc7a145
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-22 20:55:43 +0530

    Hooks

--- app14/src/Component/ChildOne.jsx
+++ app14/src/Component/ChildOne.jsx
@@ -1,3 +1,45 @@
+import { Component } from "react";
 
 
 
+export default class ChildOne extends Component{
+     constructor() {
+    super();
+    this.state = {
+      message: "",
+    };
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log("getDerivedStateFromProps triggred");
+    if (props.count < 50) {
+      return { message: "Value is Less Than 50" };
+    } else {
+      return { message: "Value Is Grater Than 50" };
+    }
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate Triggered");
+    return true;
+  }
+
+  render() {
+    console.log("Render Method Triggered");
+    return (
+      <div>
+        <h2>{this.props.count}</h2>
+        <h2>{this.state.message}</h2>
+        {this.props.count <= 50 && <UserOne />}
+      </div>
+    );
+  }
+  getSnapshotBeforeUpdate() {
+    console.log("getSnapshotBeforeUpdate Triggered");
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevProps, prevState);
+    console.log("componentDidUpdate");
+  }
+}
