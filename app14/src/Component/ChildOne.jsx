52be61bc810945c2dd34a888dd2811c299c40c47
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-23 12:03:35 +0530

    React Hooks

--- app14/src/Component/ChildOne.jsx
+++ app14/src/Component/ChildOne.jsx
@@ -1,45 +1,46 @@
 import { Component } from "react";
+import UserOne from "./UserOne"; 
 
-
-
-export default class ChildOne extends Component{
-     constructor() {
+export default class ChildOne extends Component {
+  constructor() {
     super();
     this.state = {
       message: "",
     };
   }
+
   static getDerivedStateFromProps(props, state) {
-    console.log("getDerivedStateFromProps triggred");
+    console.log("getDerivedStateFromProps triggered");
     if (props.count < 50) {
       return { message: "Value is Less Than 50" };
     } else {
-      return { message: "Value Is Grater Than 50" };
+      return { message: "Value is Greater Than 50" };
     }
   }
 
   shouldComponentUpdate() {
-    console.log("shouldComponentUpdate Triggered");
+    console.log("shouldComponentUpdate triggered");
     return true;
   }
 
   render() {
-    console.log("Render Method Triggered");
+    console.log("Render Method triggered");
     return (
       <div>
         <h2>{this.props.count}</h2>
         <h2>{this.state.message}</h2>
-        {this.props.count <= 50 && <UserOne />}
+        {this.props.count <= 50 && <UserOne />} 
       </div>
     );
   }
+
   getSnapshotBeforeUpdate() {
-    console.log("getSnapshotBeforeUpdate Triggered");
+    console.log("getSnapshotBeforeUpdate triggered");
     return null;
   }
 
   componentDidUpdate(prevProps, prevState) {
     console.log(prevProps, prevState);
-    console.log("componentDidUpdate");
+    console.log("componentDidUpdate triggered");
   }
 }
