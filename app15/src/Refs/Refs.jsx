1f4bd1443a72650c3845390628980dc6b7001553
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-09-30 20:42:23 +0530

    REFS

---
+++ app15/src/Refs/Refs.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+
+export default class Refs extends Component {
+    constructor() {
+     super();
+     this.inputRef = React.createRef();
+    }
+    render() {
+     return (
+     <div>
+      <h2>{"Welcome to React Ref Demo"}</h2>
+      <input type="text" ref={this.inputRef} />
+      </div>
+        );
+    }
+    componentDidMount() {
+        this.inputRef.current.focus();
+        this.inputRef.current.value = "Hello";
+        this.inputRef.current.style.background = "darkblue";
+        this.inputRef.current.style.color = "White";
+    }
+}
