cb38705009c6ceed7851f9fc15dfbe2991fecfbc
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-07 21:09:17 +0530

    React-js

---
+++ app16/src/Components/ComponentClass.jsx
@@ -0,0 +1,24 @@
+import { Component } from "react";
+
+
+export default class ComponentClass extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+        this.state={
+            message:"I am from component class"
+        };
+         this.state={
+        Pavan:"hi iam from pavan string"
+         }
+        return(
+            <div>
+
+                <h1>Welcome to ComponentClass</h1>
+                <h2>{this.state.Pavan}</h2>
+           <h2>{this.state.message}</h2>
+            </div>
+        )
+    }
+}
