75a909400a3a6c528c381c0eedcb02c1205938c1
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-15 21:06:15 +0530

    HOC

---
+++ revise/src/Components/Hoc/NameInput.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WithInputTracker from "./WithInputTracker";
+
+function NameInput({text,handleChange}){
+return(
+    <div style={{margin:"20px",textAlign:"center"}}>
+        <h3>Enter Your Name:</h3>
+        <input type="text" placeholder="Type Your Name" value={text} onChange={handleChange}/>
+        <p>Length:{text.length}</p>
+    </div>
+);
+}
+export default WithInputTracker(NameInput);
