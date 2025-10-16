bd67d3846030725c8c9e51eb4ef17b8980e2ada2
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-16 20:57:49 +0530

    HOC

--- revise/src/Components/Hoc/CommentInput.jsx
+++ revise/src/Components/Hoc/CommentInput.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import WithInputTracker from "./WithInputTracker";
 
-function CommentInput({text,handleChange}){
+function CommentInput({ text, handleChange }) {
     return (
-        <div style={{margin:"20px",textAlign:"Center"}}>
+        <div style={{ margin: "20px", textAlign: "Center" }}>
             <><h3>Leave a Comment:</h3><textarea rows={4} cols={30} placeholder="Write Your Comment"
                 value={text} onChange={handleChange} /></>
-                <p>Character Count:{text.length}</p>
+            <p>Character Count:{text.length}</p>
         </div>
     );
 }
