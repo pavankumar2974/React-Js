e6b57d9a56913b7ac175aabcf78a070eda479d47
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-28 15:22:35 +0530

    Refs

--- revise/src/App.jsx
+++ revise/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import NameInput from './Components/Hoc/NameInput'
 import CommentInput from './Components/Hoc/CommentInput'
+import Refs from './Components/Refs/Refs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +12,10 @@ function App() {
   return (
     <>
       <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>React Hoc-Input Length Tracker</h1>
-        <NameInput />
-        <CommentInput />
+        {/* <h1>React Hoc-Input Length Tracker</h1> */}
+        {/* <NameInput />
+        <CommentInput /> */}
+        <Refs/>
       </div>
     </>
   );
--- revise/src/Components/Refs/Refs.jsx
+++ revise/src/Components/Refs/Refs.jsx
@@ -1,2 +1,23 @@
+import React, { Component } from "react";
 
 
+export default class Refs extends Component{
+    constructor(){
+        super();
+        this.inputRef=React.createRef();
+    }
+    render(){
+        return(
+            <div>
+                <h2>{"Welcome to React Ref "}</h2>
+                <input type="text" ref={this.inputRef} />
+            </div>
+        );
+    }
+    componentDidMount(){
+        this.inputRef.current.focus();
+        this.inputRef.current.value = "Hello";
+    this.inputRef.current.style.background = "red";
+    this.inputRef.current.style.color = "Yellow";
+    }
+}
