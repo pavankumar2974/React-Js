510e7eedd275c34e8413af4ecc29a520ea56460c
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-28 21:47:48 +0530

    LifeCycleHooks

--- revise/src/Components/LifeCycleHooks/LifeCycleHooks.jsx
+++ revise/src/Components/LifeCycleHooks/LifeCycleHooks.jsx
@@ -1,2 +1,40 @@
 
 
+import { Component } from "react";
+
+import ChildComp from "./ChildComp";
+
+
+
+export default class LifeCycleHooks extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 10,
+        };
+    }
+
+    incrementCount = () => {
+        this.setState({ count: this.state.count + 10 });
+    };
+    decrementCount = () => {
+        this.setState({ count: this.state.count - 10 });
+    };
+    multipleCount = () => {
+        this.setState({ count: this.state.count * 2 });
+    };
+    divide = () => {
+        this.setState({ count: this.state.count / 2 });
+    };
+    render() {
+        return (
+            <div>
+                <ChildComp count={this.state.count} />
+                <button onClick={this.incrementCount}>Increment</button>
+                <button onClick={this.decrementCount}>Decrement</button>
+                <button onClick={this.multipleCount}>Multiple</button>
+                <button onClick={this.divide}>Divide</button>
+            </div>
+        )
+    }
+}
