8c79a429d2b49adb98860520a185475a9058cc75
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-28 21:47:52 +0530

    LifeCycleHooks

--- revise/src/Components/LifeCycleHooks/UserComp.jsx
+++ revise/src/Components/LifeCycleHooks/UserComp.jsx
@@ -1,2 +1,18 @@
 
 
+import { Component } from "react";
+
+
+export default class UserComp extends Component {
+    render() {
+        return (
+            <div>
+                <img src="https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg" alt="" />
+
+            </div>
+        );
+    }
+    componentWillUnmount() {
+        alert("since you reached the limit, The Image will be removed");
+    }
+}
