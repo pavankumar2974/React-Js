076e2ad1c2b235d59bb51111c2dd28c9df4739d2
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-28 21:47:40 +0530

    LifeCycleHooks

--- revise/src/Components/LifeCycleHooks/ChildComp.jsx
+++ revise/src/Components/LifeCycleHooks/ChildComp.jsx
@@ -1,2 +1,49 @@
 
 
+import { Component } from "react";
+import UserComp from "./UserComp";
+
+export default class ChildComp extends Component {
+    constructor() {
+        super();
+        this.state = {
+            message: "",
+        };
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        console.log("getDerivedStateFromProps Triggered");
+        if (props.count < 50) {
+            return { message: "Value is less than 50" };
+        } else {
+            return { message: "Value is greater than 50" };
+        }
+    }
+
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate Triggered");
+        return true;
+    }
+
+    render() {
+        console.log("Render Method Triggered");
+        return (
+            <div>
+                <h2>{this.props.count}</h2>
+                <h2>{this.state.message}</h2>
+                {this.props.count <= 50 && <UserComp />}
+            </div>
+        );
+    }
+
+    getSnapshotBeforeUpdate() {
+        console.log("getSnapshotBeforeUpdate Triggered");
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log("Previous Props:", prevProps);
+        console.log("Previous State:", prevState);
+        console.log("componentDidUpdate Triggered");
+    }
+}
