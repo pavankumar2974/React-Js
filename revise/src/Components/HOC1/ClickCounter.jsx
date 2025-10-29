6a76db9d6e00ca438bfea7df898f7c76af6f4af0
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-29 21:16:27 +0530

    HOC

--- revise/src/Components/HOC1/ClickCounter.jsx
+++ revise/src/Components/HOC1/ClickCounter.jsx
@@ -2,18 +2,18 @@ import { Component } from "react";
 import HocComponent from "./HocComponent";
 
 
-class ClickCounter extends Component{
-    render(){
+class ClickCounter extends Component {
+    render() {
         console.log(this.props);
         console.log("FromClickCounter");
-        const{count,incrementCount,decrementCount,resetCount}=this.props;
-       return(
+        const { count, incrementCount, decrementCount, resetCount } = this.props;
+        return (
             <div>
-<button onClick={incrementCount}>Increment</button>
-<button onClick={decrementCount}>Decrement</button>
+                <button onClick={incrementCount}>Increment</button>
+                <button onClick={decrementCount}>Decrement</button>
 
-<button onClick={resetCount}>Reset</button>
-<h2>Count:{count}</h2>
+                <button onClick={resetCount}>Reset</button>
+                <h2>Count:{count}</h2>
             </div>
         )
     }
