84fcddf8f1592768e9006a690240dc720ce2c0d1
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-30 20:45:35 +0530

    Refs

--- revise/src/Components/Refs/RefsDemo.jsx
+++ revise/src/Components/Refs/RefsDemo.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 
 
-export default class RefsDemo extends Component{
-    constructor(){
+export default class RefsDemo extends Component {
+    constructor() {
         super();
-        this.inputRef=React.createRef();
+        this.inputRef = React.createRef();
     }
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <h2>React Ref Demo Welcomes you</h2>
-                <textarea ref={this.inputRef}/>
+                <textarea ref={this.inputRef} />
             </div>
         );
     }
-    componentDidMount(){
+    componentDidMount() {
         this.inputRef.current.focus();
-        this.inputRef.current.value="Hello";
-        this.inputRef.current.style.background="Black";
-        this.inputRef.current.style.color="Red";
+        this.inputRef.current.value = "Hello";
+        this.inputRef.current.style.background = "Black";
+        this.inputRef.current.style.color = "Red";
     }
 }
