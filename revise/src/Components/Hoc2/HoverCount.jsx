0b9c55784c2ab0b8f3c84cad509d6892936f27ad
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-30 20:45:07 +0530

    HOC

---
+++ revise/src/Components/Hoc2/HoverCount.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import HocComp from "./HocComp";
+
+
+
+class HoverCount extends Component{
+    render(){
+        // console.log(this.props);
+        const{count,incrementCount,decrementCount,resetCount}=this.props;
+        
+        return(
+            <div>
+                <button onMouseOver={incrementCount}>Increment</button>
+                <button onMouseOver={decrementCount}>Decrement</button>
+                <button onMouseOver={resetCount}>Reset</button>
+                <h2>Count:{count}</h2>
+            </div>
+        )
+    }
+}
+export default HocComp(HoverCount);
