76a4e79100853ec3f636c90329506f0fe5f46b48
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-30 20:45:13 +0530

    Refs

---
+++ revise/src/Components/Refs/RefsDemo.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+
+export default class RefsDemo extends Component{
+    constructor(){
+        super();
+        this.inputRef=React.createRef();
+    }
+    render(){
+        return(
+            <div>
+                <h2>React Ref Demo Welcomes you</h2>
+                <textarea ref={this.inputRef}/>
+            </div>
+        );
+    }
+    componentDidMount(){
+        this.inputRef.current.focus();
+        this.inputRef.current.value="Hello";
+        this.inputRef.current.style.background="Black";
+        this.inputRef.current.style.color="Red";
+    }
+}
