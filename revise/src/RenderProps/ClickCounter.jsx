89947af65c3fa771762ef2814bb856bb7a8c0976
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-31 20:31:19 +0530

    RenderProps

---
+++ revise/src/RenderProps/ClickCounter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class ClickCounter extends Component {
+  render() {
+    const { count, incrementCount, decrementCount, resetCount } = this.props;
+    return (
+      <div>
+        <h3>Click Counter</h3>
+        <button onClick={incrementCount}>Increment</button>
+        <button onClick={decrementCount}>Decrement</button>
+        <button onClick={resetCount}>Reset</button>
+        <p>Count: {count}</p>
+      </div>
+    );
+  }
+}
