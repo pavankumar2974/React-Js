f89140ce666ffcad549c575ae0e1b83763dae046
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-10-31 20:31:12 +0530

    RenderProps

--- revise/src/App.jsx
+++ revise/src/App.jsx
@@ -1,45 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import NameInput from './Components/Hoc/NameInput'
-import CommentInput from './Components/Hoc/CommentInput'
-import Refs from './Components/Refs/Refs'
-import ClickCounter from './Components/HOC1/ClickCounter'
-import HoverCounter from './Components/HOC1/HoverCounter'
-import ChildComp from './Components/LifeCycleHooks/ChildComp'
-import LifeCycleHooks from './Components/LifeCycleHooks/LifeCycleHooks'
-import UserComp from './Components/LifeCycleHooks/UserComp'
-import RefsDemo from './Components/Refs/RefsDemo'
-import HoverCount from './Components/Hoc2/HoverCount'
-import ClickCount from './Components/Hoc2/ClickCount'
+import "./App.css";
+import ClickCounter from "./Components/ClickCounter";
+import HoverCounter from "./Components/HoverCounter";
+import HandleCounter from "./Components/HandleCounter";
+import ShowMessage from "./Components/ShowMessage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        {/* <h1>React Hoc-Input Length Tracker</h1> */}
-        {/* <NameInput />
-        <CommentInput /> */}
-        {/* <Refs/> */}
-        {/* <ClickCounter/>
-        <br/>
-        <hr/>
-        <HoverCounter/> */}
-        {/* <ChildComp/>
-        <LifeCycleHooks/>
-        <UserComp/> */}
-        {/* <RefsDemo/> */}
-        <ClickCount/>
-        <br/>
-        <hr/>
-        <HoverCount/>
+      {/* Using Render Props with ClickCounter */}
+      <HandleCounter
+        render={(count, incrementCount, decrementCount, resetCount) => (
+          <ClickCounter
+            count={count}
+            incrementCount={incrementCount}
+            decrementCount={decrementCount}
+            resetCount={resetCount}
+          />
+        )}
+      />
 
-      </div>
+      {/* Using Render Props with HoverCounter */}
+      <HandleCounter
+        render={(count, incrementCount, decrementCount, resetCount) => (
+          <HoverCounter
+            count={count}
+            incrementCount={incrementCount}
+            decrementCount={decrementCount}
+            resetCount={resetCount}
+          />
+        )}
+      />
+
+      {/* Optional example of ShowMessage usage */}
+      <ShowMessage msg="Render Props Example Working Successfully!" />
     </>
   );
 }
 
-export default App
+export default App;
