d1dbb1eb02cfd489ab06cde62fa3b440c623fefa
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-01 20:30:54 +0530

    RenderProps

--- revise/src/RenderProps/HoverCounter.jsx
+++ revise/src/RenderProps/HoverCounter.jsx
@@ -1,15 +1,15 @@
 import { Component } from "react";
 
 export default class HoverCounter extends Component {
-  render() {
-    const { count, incrementCount, decrementCount, resetCount } = this.props;
-    return (
-      <div>
-        <button onMouseOver={incrementCount}>Increment (Hover)</button>
-        <button onClick={decrementCount}>Decrement</button>
-        <button onClick={resetCount}>Reset</button>
-        <p>Count: {count}</p>
-      </div>
-    );
-  }
+    render() {
+        const { count, incrementCount, decrementCount, resetCount } = this.props;
+        return (
+            <div>
+                <button onMouseOver={incrementCount}>Increment (Hover)</button>
+                <button onClick={decrementCount}>Decrement</button>
+                <button onClick={resetCount}>Reset</button>
+                <p>Count: {count}</p>
+            </div>
+        );
+    }
 }
