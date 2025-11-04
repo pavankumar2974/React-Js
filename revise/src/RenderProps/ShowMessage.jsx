6558ef64dddc823bc67cc1dec45901c3ebc72247
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-04 14:28:24 +0530

    RenderProps

--- revise/src/RenderProps/ShowMessage.jsx
+++ revise/src/RenderProps/ShowMessage.jsx
@@ -1,14 +1,12 @@
 import { Component } from "react";
 
 
-export default class ShowMessage extends Component {
-    render() {
-        return (
+export default class ShowMessage extends Component{
+    render(){
+        return(
             <div>
-                {/* <h2>{this.props.hello}</h2>
-        <p> {this.props.sayHello()}</p> */}
-                <p>Iam From Show Component</p>
-                <hr />
+                <p>I am from Show Component</p>
+                <hr/>
                 <h2>{this.props.msg}</h2>
             </div>
         );
