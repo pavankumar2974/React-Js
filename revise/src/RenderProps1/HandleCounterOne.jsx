54a7955d72ec56ab70afd4527aaec4524022361b
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-05 21:06:27 +0530

    RenderProps

---
+++ revise/src/RenderProps1/HandleCounterOne.jsx
@@ -0,0 +1,32 @@
+import { Component } from "react";
+
+
+export default class HandleCounterOne extends Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            count:0,
+        };
+    }
+    incrementCount=()=>{
+        this.setState({count:this.state.count+1});
+    };
+    decrementCount=()=>{
+        this.setState({count:this.state.count-1});
+    };
+    resetCount=()=>{
+        this.setState({count:0});
+    };
+    render(){
+        return(
+            <div>
+                {this.props.render(
+                    this.state.count,
+                    this.incrementCount,this.decrementCount,this.resetCount
+                )}
+            </div>
+        )
+    }
+
+}
