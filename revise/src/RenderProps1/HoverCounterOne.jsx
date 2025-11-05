124800640603dbeffd8ffcad3eec2e1087c5f43d
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-05 21:06:32 +0530

    RenderProps

---
+++ revise/src/RenderProps1/HoverCounterOne.jsx
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+
+export default class HoverCounterOne extends Component{
+    render(){
+        const {count,incrementCount,decrementCount,resetCount}=this.props;
+        return(
+            <div>
+                <button onMouseOver={incrementCount}>Increment</button>
+                <button onMouseOver={decrementCount}>Decrement</button>
+                <button onMouseOver={resetCount}>Reset</button>
+                <p>Count:{count}</p>
+
+            </div>
+        )
+    }
+}
