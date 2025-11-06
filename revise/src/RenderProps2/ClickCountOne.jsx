e7fa28788352f0e3ffa9959fd02c99e47adb5e6e
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-06 20:22:26 +0530

    RenderProps

--- revise/src/RenderProps2/ClickCountOne.jsx
+++ revise/src/RenderProps2/ClickCountOne.jsx
@@ -1,10 +1,10 @@
 import { Component } from "react";
 
 
-export default class ClickCountOne extends Component{
-    render(){
-        const {count,incrementCount,decrementCount,resetCount}=this.props;
-        return(
+export default class ClickCountOne extends Component {
+    render() {
+        const { count, incrementCount, decrementCount, resetCount } = this.props;
+        return (
             <div>
                 <button onClick={incrementCount}>Increment</button>
                 <button onClick={decrementCount}>Decrement</button>
