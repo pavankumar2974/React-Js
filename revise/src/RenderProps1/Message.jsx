a8f4014e8b06b479f16694ccde0b519d35657607
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-11 18:44:37 +0530

    React-js

--- revise/src/RenderProps1/Message.jsx
+++ revise/src/RenderProps1/Message.jsx
@@ -1,2 +1,16 @@
+import { Component } from "react";
 
 
+export default class Message extends Component{
+    render(){
+        return(
+            <div>
+                <h2>{this.props.hello}</h2>
+                <p>{this.props.sayHello}</p>
+                <p>I am from message Component</p>
+                <hr/>
+                <h2>{this.props.msg}</h2>
+            </div>
+        )
+    }
+}
