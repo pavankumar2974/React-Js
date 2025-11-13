59530c92a6745121f80867d0447bf160a75102e3
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-13 21:57:55 +0530

    HOCComponent

---
+++ app18/src/HocComponent/HocComp.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+
+
+function HocComp(MiddleComp){
+    return class extends Component{
+        constructor(props){
+            super(props);
+            this.state={
+                count:0,
+            };
+        }
+        incrementCount=()=>{
+            this.setState({count:this.state.count+1});
+        };
+        decrementCount=()=>{
+            this.setState({count:this.state.count-1});
+        };
+        resetCount=()=>{
+            this.setState({count:0
+
+            });
+        };
+        render(){
+            return(
+                <div>
+                    <MiddleComp 
+                    incrementCount={this.incrementCount}
+                    decrementCount={this.decrementCount}
+                    resetCount={this.resetCount}
+                    count={this.state.count}
+                    {...this.props}/>
+                </div>
+            )
+        }
+
+    }
+}
+export default HocComp;
