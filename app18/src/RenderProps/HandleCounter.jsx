1517f4f9708498075226f25f4f918f9378ce9aed
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-14 15:29:27 +0530

    RenderProps

--- app18/src/RenderProps/HandleCounter.jsx
+++ app18/src/RenderProps/HandleCounter.jsx
@@ -4,6 +4,30 @@ import { Component } from "react";
 export default class HandleCounter extends Component{
     constructor(props){
         super(props);
-        this.state={}
+        this.state={
+            count:0,
+        }
     }
+    increse=()=>{
+        this.setState({count:this.state.count+1})
+    }
+    decrese=()=>{
+        this.setState({count:this.state.count-1})
+    }
+    reset=()=>{
+        this.setState({count:0})
+    }
+    render(){
+        return(
+            <div>
+                {this.props.render(
+                    this.state.count,
+                    this.increse,
+                    this.decrese,
+                    this.reset,
+                )}
+            </div>
+        )
+    }
+
 }
