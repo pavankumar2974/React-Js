ec6bd1d2ac24e6c42993b938435bf22c69ae5718
pavankumar2964 <pavankumar29ctr@gmail.com>
2025-11-14 15:29:02 +0530

    RenderProps

--- app18/src/App.jsx
+++ app18/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import ClickCount from './HocComponent/ClickCount'
 import HoverCount from './HocComponent/HoverCount'
 import HandleCounter from './RenderProps/HandleCounter'
+import ClickCounterTwo from './RenderProps/ClickCounterTwo'
+import HoverCounter from './RenderProps/HoverCounter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,8 +19,26 @@ function App() {
         <hr/>
         <HoverCount/> */}
 <hr/>
-<HandleCounter 
-render
+
+<HandleCounter
+render={(count,increse,decrese,reset)=>(
+  <ClickCounterTwo
+  count={count}
+  increse={increse}
+  decrese={decrese}
+  reset={reset}/>
+)}/>
+<hr/>
+<br/>
+<HandleCounter
+render={(count,increse,decrese,reset)=>(
+<HoverCounter
+count={count}
+increse={increse}
+decrese={decrese}
+reset={reset}/>
+)}
+  
 />
 
       </div>
